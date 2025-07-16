48e4004174b070fd3ed60d0af12c6a5b0a2c86c4
alivilla01 <alisonvillalta01@gmail.com>
2025-07-16 10:20:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,116 +1,120 @@
-let aprobados = new Set();
 const STORAGE_KEY = 'mallaAprobadosAli';
+let aprobados = new Set();
 
-// ================================
-// ✅ Tu malla completa aquí:
 const malla = [
   {
     ciclo: "I Semestre",
     cursos: [
-      { id: "EG-I", nombre: "Curso Integrado de Humanidades I", requisitos: [] },
-      { id: "EG-", nombre: "Curso de Arte", requisitos: [] },
-      { id: "RP-", nombre: "Repertorio", requisitos: [] },
-      { id: "DN-0101", nombre: "Introducción a la Administración de Negocios", requisitos: [] },
-      { id: "MA-0001", nombre: "Precálculo", requisitos: [] },
-      { id: "DN-0102", nombre: "Aplicaciones Ofimáticas para la Toma de Decisiones", requisitos: [] }
+      { id: "EG-I", nombre: "Curso Integrado de Humanidades I", req: [] },
+      { id: "EG-", nombre: "Curso de Arte", req: [] },
+      { id: "RP-", nombre: "Repertorio", req: [] },
+      { id: "DN-0101", nombre: "Introducción a la Administración de Negocios", req: [] },
+      { id: "MA-0001", nombre: "Precálculo", req: [] },
+      { id: "DN-0102", nombre: "Aplicaciones Ofimáticas para la Toma de Decisiones", req: [] }
     ]
   },
   {
     ciclo: "II Semestre",
     cursos: [
-      { id: "EG-II", nombre: "Curso Integrado de Humanidades II", requisitos: ["EG-I"] },
-      { id: "EF-", nombre: "Actividad Deportiva", requisitos: [] },
-      { id: "DN-0104", nombre: "Elementos Fundamentales de Legislación Empresarial", requisitos: ["DN-0101"] },
-      { id: "DN-0103", nombre: "Administración de Proyectos y Herramientas para el Análisis de Datos", requisitos: ["DN-0102"] },
-      { id: "MA-1021", nombre: "Cálculo para Ciencias Económicas I", requisitos: ["MA-0001"] }
+      { id: "EG-II", nombre: "Curso Integrado de Humanidades II", req: ["EG-I"] },
+      { id: "EF-", nombre: "Actividad Deportiva", req: [] },
+      { id: "DN-0104", nombre: "Elementos Fundamentales de Legislación Empresarial", req: ["DN-0101"] },
+      { id: "DN-0103", nombre: "Administración de Proyectos y Herramientas para el Análisis de Datos", req: ["DN-0102"] },
+      { id: "MA-1021", nombre: "Cálculo para Ciencias Económicas I", req: ["MA-0001"] }
     ]
   },
   {
     ciclo: "III Semestre",
     cursos: [
-      { id: "PC-0200", nombre: "Contabilidad Básica", requisitos: ["DN-0101"] },
-      { id: "PC-0240", nombre: "Matemática Financiera", requisitos: ["MA-1021"] },
-      { id: "PC-0261", nombre: "Legislación Comercial, Bancaria y Financiera", requisitos: ["DN-0104"] },
-      { id: "XS-0276", nombre: "Estadística General I", requisitos: ["MA-1021"] },
-      { id: "MA-1022", nombre: "Cálculo para Ciencias Económicas II", requisitos: ["MA-1021"] },
-      { id: "OPT1231", nombre: "Opcional", requisitos: [] }
+      { id: "PC-0200", nombre: "Contabilidad Básica", req: ["DN-0101"] },
+      { id: "PC-0240", nombre: "Matemática Financiera", req: ["MA-1021"] },
+      { id: "PC-0261", nombre: "Legislación Comercial, Bancaria y Financiera", req: ["DN-0104"] },
+      { id: "XS-0276", nombre: "Estadística General I", req: ["MA-1021"] },
+      { id: "MA-1022", nombre: "Cálculo para Ciencias Económicas II", req: ["MA-1021"] },
+      { id: "OPT1231", nombre: "Opcional", req: [] }
     ]
   },
   {
     ciclo: "IV Semestre",
     cursos: [
-      { id: "SR-I", nombre: "Seminario de Realidad Nacional I", requisitos: ["EG-II"] },
-      { id: "PC-0260", nombre: "Legislación Laboral", requisitos: ["PC-0261"] },
-      { id: "PC-0202", nombre: "Contabilidad Intermedia I", requisitos: ["PC-0200"] },
-      { id: "DN-0123", nombre: "Metodología de la Investigación", requisitos: ["XS-0276"] },
-      { id: "XS-0277", nombre: "Estadística General II", requisitos: ["XS-0276", "MA-1022"] },
-      { id: "DN-0340", nombre: "Administración Financiera I", requisitos: ["PC-0240", "PC-0200"] }
+      { id: "SR-I", nombre: "Seminario de Realidad Nacional I", req: ["EG-II"] },
+      { id: "PC-0260", nombre: "Legislación Laboral", req: ["PC-0261"] },
+      { id: "PC-0202", nombre: "Contabilidad Intermedia I", req: ["PC-0200"] },
+      { id: "DN-0123", nombre: "Metodología de la Investigación", req: ["XS-0276"] },
+      { id: "XS-0277", nombre: "Estadística General II", req: ["XS-0276", "MA-1022"] },
+      { id: "DN-0340", nombre: "Administración Financiera I", req: ["PC-0240", "PC-0200"] }
     ]
   },
-  {
+    {
     ciclo: "V Semestre",
     cursos: [
-      { id: "SR-II", nombre: "Seminario De Realidad Nacional II", requisitos: ["SR-I"] },
-      { id: "PC-0304", nombre: "Contabilidad Intermedia II", requisitos: ["PC-0202", "PC-0240"] },
-      { id: "PC-0320", nombre: "Auditoría Financiera I", requisitos: ["PC-0202", "XS-0276"] },
-      { id: "PC-0241", nombre: "Negocios Y Entorno Económico", requisitos: ["PC-0261"] },
-      { id: "PC-0212", nombre: "Gerencia Y Liderazgo Para Contadores", requisitos: ["DN-0101"] },
-      { id: "DN-0341", nombre: "Administración Financiera II", requisitos: ["DN-0340", "XS-0277"] }
+      { id: "SR-II", nombre: "Seminario De Realidad Nacional II", req: ["SR-I"] },
+      { id: "PC-0304", nombre: "Contabilidad Intermedia II", req: ["PC-0202", "PC-0240"] },
+      { id: "PC-0320", nombre: "Auditoría Financiera I", req: ["PC-0202", "XS-0276"] },
+      { id: "PC-0241", nombre: "Negocios Y Entorno Económico", req: ["PC-0261"] },
+      { id: "PC-0212", nombre: "Gerencia Y Liderazgo Para Contadores", req: ["DN-0101"] },
+      { id: "DN-0341", nombre: "Administración Financiera II", req: ["DN-0340", "XS-0277"] }
     ]
   },
   {
     ciclo: "VI Semestre",
     cursos: [
-      { id: "DN-0105", nombre: "Métodos Cuantitativos Para La Toma De Decisiones I", requisitos: ["DN-0340", "XS-0277"] },
-      { id: "DN-0320", nombre: "Principios De Mercadeo", requisitos: ["PC-0200", "XS-0276"] },
-      { id: "PC-0211", nombre: "Muestreo Aplicado A La Auditoría", requisitos: ["PC-0320", "XS-0277"] },
-      { id: "PC-0242", nombre: "Entorno, Gestión Y Control", requisitos: ["PC-0241"] },
-      { id: "PC-0305", nombre: "Contabilizaciones Especiales", requisitos: ["PC-0304"] },
-      { id: "PC-0321", nombre: "Auditoría Financiera II", requisitos: ["PC-0320"] }
+      { id: "DN-0105", nombre: "Métodos Cuantitativos Para La Toma De Decisiones I", req: ["DN-0340", "XS-0277"] },
+      { id: "DN-0320", nombre: "Principios De Mercadeo", req: ["PC-0200", "XS-0276"] },
+      { id: "PC-0211", nombre: "Muestreo Aplicado A La Auditoría", req: ["PC-0320", "XS-0276"] },
+      { id: "PC-0242", nombre: "Entorno, Gestión Y Control", req: ["PC-0241"] },
+      { id: "PC-0305", nombre: "Contabilizaciones Especiales", req: ["PC-0304"] },
+      { id: "PC-0321", nombre: "Auditoría Financiera II", req: ["PC-0320"] }
     ]
   },
   {
     ciclo: "VII Semestre",
     cursos: [
-      { id: "PC-0204", nombre: "Laboratorio De Contabilidad", requisitos: ["PC-0305"] },
-      { id: "PC-0306", nombre: "Sistemas De Costeo Básico", requisitos: ["PC-0304"] },
-      { id: "PC-0344", nombre: "Formulación Y Evaluación De Proyectos I", requisitos: ["DN-0341"] },
-      { id: "PC-0407", nombre: "Contabilidad Avanzada I", requisitos: ["PC-0305"] },
-      { id: "PC-0421", nombre: "Auditoría Financiera III", requisitos: ["PC-0321"] },
-      { id: "PC-0462", nombre: "Legislación Tributaria Y Aduanera", requisitos: ["PC-0260", "PC-0304"] }
+      { id: "PC-0204", nombre: "Laboratorio De Contabilidad", req: ["PC-0305"] },
+      { id: "PC-0306", nombre: "Sistemas De Costeo Básico", req: ["PC-0304"] },
+      { id: "PC-0344", nombre: "Formulación Y Evaluación De Proyectos I", req: ["DN-0341"] },
+      { id: "PC-0407", nombre: "Contabilidad Avanzada I", req: ["PC-0305"] },
+      { id: "PC-0421", nombre: "Auditoría Financiera III", req: ["PC-0321"] },
+      { id: "PC-0462", nombre: "Legislación Tributaria Y Aduanera", req: ["PC-0260", "PC-0304"] }
     ]
   },
   {
     ciclo: "VIII Semestre",
     cursos: [
-      { id: "DN-0110", nombre: "Métodos Cuantitativos Para La Toma De Decisiones II", requisitos: ["DN-0105"] },
-      { id: "DN-0525", nombre: "Mercadeo De Servicios", requisitos: ["DN-0320"] },
-      { id: "PC-0205", nombre: "Contabilidad Gubernamental", requisitos: ["PC-0407"] },
-      { id: "PC-0409", nombre: "Sistemas De Costeo Gerencial", requisitos: ["PC-0306"] },
-      { id: "PC-0410", nombre: "Contabilidad Avanzada II", requisitos: ["PC-0407"] },
-      { id: "PC-0531", nombre: "Auditoría Financiera IV", requisitos: ["PC-0421"] }
+      { id: "DN-0110", nombre: "Métodos Cuantitativos Para La Toma De Decisiones II", req: ["DN-0105"] },
+      { id: "DN-0525", nombre: "Mercadeo De Servicios", req: ["DN-0320"] },
+      { id: "PC-0205", nombre: "Contabilidad Gubernamental", req: ["PC-0407"] },
+      { id: "PC-0409", nombre: "Sistemas De Costeo Gerencial", req: ["PC-0306"] },
+      { id: "PC-0410", nombre: "Contabilidad Avanzada II", req: ["PC-0407"] },
+      { id: "PC-0531", nombre: "Auditoría Financiera IV", req: ["PC-0421"] }
     ]
   },
-  {
+    {
     ciclo: "IX Semestre",
     cursos: [
-      { id: "DN-0114", nombre: "Comunicación Intercultural De Los Negocios", requisitos: ["PC-0409"] },
-      { id: "OPT-270", nombre: "Bloque Optativo", requisitos: [] },
-      { id: "PC-0423", nombre: "Auditoría Informática I", requisitos: ["PC-0531"] },
-      { id: "PC-0424", nombre: "Laboratorio De Auditoría Informática I", requisitos: ["PC-0531"] },
-      { id: "PC-0425", nombre: "Control Interno Y Auditorías Especiales", requisitos: ["PC-0531"] },
-      { id: "PC-0528", nombre: "Auditoría De Gestión I", requisitos: ["PC-0421"] }
+      { id: "DN-0114", nombre: "Comunicación Intercultural De Los Negocios", req: ["PC-0409"] },
+      { id: "OPT-270", nombre: "Bloque Optativo", req: [] },
+      { id: "PC-0423", nombre: "Auditoría Informática I", req: [] },
+      { id: "PC-0424", nombre: "Laboratorio De Auditoría Informática I", req: [] },
+      { id: "PC-0425", nombre: "Control Interno Y Auditorías Especiales", req: [] },
+      { id: "PC-0528", nombre: "Auditoría De Gestión I", req: ["PC-0421"] }
     ]
   },
   {
     ciclo: "X Semestre",
     cursos: [
-      { id: "DN-0115", nombre: "Taller De Investigación", requisitos: ["DN-0114"] },
-      { id: "OPT-1155", nombre: "Bloque para Siglas de TFG", requisitos: [] },
-      { id: "PC-0210", nombre: "Auditoría Interna", requisitos: ["PC-0425"] },
-      { id: "PC-0526", nombre: "Auditoría Informática II", requisitos: ["PC-0423", "PC-0424"] },
-      { id: "PC-0527", nombre: "Laboratorio De Auditoría Informática II", requisitos: ["PC-0423", "PC-0424"] },
-      { id: "PC-0529", nombre: "Auditoría De Gestión II", requisitos: ["PC-0528"] }
+      { id: "DN-0115", nombre: "Taller De Investigación", req: [] },
+      { id: "OPT-270", nombre: "Bloque Optativo", req: [] },
+      { id: "PC-0210", nombre: "Auditoría Interna", req: ["PC-0425"] },
+      { id: "PC-0526", nombre: "Auditoría Informática II", req: ["PC-0423", "PC-0424"] },
+      { id: "PC-0527", nombre: "Laboratorio De Auditoría Informática II", req: ["PC-0423", "PC-0424"] },
+      { id: "PC-0529", nombre: "Auditoría De Gestión II", req: ["PC-0528"] }
+    ]
+  },
+  {
+    ciclo: "XI Semestre",
+    cursos: [
+      { id: "OPT-1155", nombre: "Bloque Para Siglas De TFG", req: [] }
     ]
   }
 ];
@@ -120,6 +124,8 @@ const malla = [
 function generarMalla() {
   const contenedor = document.getElementById("contenedor-ciclos");
   contenedor.innerHTML = "";
+  const frag = document.createDocumentFragment();
+
   malla.forEach(bloque => {
     const divCiclo = document.createElement("div");
     divCiclo.className = "ciclo";
@@ -133,43 +139,40 @@ function generarMalla() {
       divCurso.id = curso.id;
       divCurso.innerHTML = `
         <h3>${curso.nombre}</h3>
-        <p><strong>Requisitos:</strong> ${curso.requisitos.length ? curso.requisitos.join(", ") : "Ninguno"}</p>
+        <p><strong>Requisitos:</strong> ${curso.req.length ? curso.req.join(", ") : "Ninguno"}</p>
         <label class="aprobado">
-          <input type="checkbox" onchange="marcarAprobado('${curso.id}')" id="check-${curso.id}"> Aprobado
+          <input type="checkbox" id="check-${curso.id}"> Aprobado
         </label>`;
       contCursos.appendChild(divCurso);
     });
 
     divCiclo.appendChild(contCursos);
-    contenedor.appendChild(divCiclo);
+    frag.appendChild(divCiclo);
   });
-  actualizarEstado();
+
+  contenedor.appendChild(frag);
   restaurarChecks();
+  actualizarEstado();
 }
 
 function marcarAprobado(sigla) {
-  const checkbox = document.getElementById(`check-${sigla}`);
-  if (checkbox.checked) {
-    aprobados.add(sigla);
-  } else {
-    aprobados.delete(sigla);
-  }
+  const cb = document.getElementById(`check-${sigla}`);
+  cb.checked ? aprobados.add(sigla) : aprobados.delete(sigla);
   actualizarEstado();
 }
 
 function actualizarEstado() {
-  malla.forEach(bloque => {
+  malla.forEach(bloque =>
     bloque.cursos.forEach(curso => {
       const div = document.getElementById(curso.id);
-      const requisitos = curso.requisitos;
-      const habilitado = requisitos.every(r => aprobados.has(r));
-      if (habilitado || requisitos.length === 0 || aprobados.has(curso.id)) {
+      const enabled = curso.req.every(r => aprobados.has(r));
+      if (enabled || aprobados.has(curso.id) || curso.req.length === 0) {
         div.classList.remove("bloqueado");
       } else {
         div.classList.add("bloqueado");
       }
-    });
-  });
+    })
+  );
 }
 
 function guardarEstado() {
@@ -185,8 +188,8 @@ function limpiarEstado() {
 }
 
 function restaurarChecks() {
-  const guardados = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
-  guardados.forEach(id => {
+  const saved = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+  saved.forEach(id => {
     aprobados.add(id);
     const cb = document.getElementById(`check-${id}`);
     if (cb) cb.checked = true;
@@ -196,6 +199,13 @@ function restaurarChecks() {
 
 document.addEventListener("DOMContentLoaded", () => {
   generarMalla();
+  document.getElementById("contenedor-ciclos").addEventListener("change", e => {
+    if (e.target.matches('input[type="checkbox"]')) {
+      marcarAprobado(e.target.id.replace('check-', ''));
+    }
+  });
   document.getElementById("guardar").addEventListener("click", guardarEstado);
   document.getElementById("limpiar").addEventListener("click", limpiarEstado);
 });
+
+
